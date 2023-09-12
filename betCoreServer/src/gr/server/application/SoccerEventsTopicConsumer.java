6735bd2e9f95ebccf2d86a4af3e4be8371724866
liakos86 <liakos86@gmail.com>
2023-09-12 21:16:54 +0300

    almost all leagues in json files. consumer topic test

---
+++ betCoreServer/src/gr/server/application/SoccerEventsTopicConsumer.java
@@ -0,0 +1,108 @@
+package gr.server.application;
+
+import java.util.Map;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import com.google.gson.Gson;
+
+import gr.server.data.constants.ActiveMqConstants;
+import gr.server.data.live.helper.LiveUpdatesHelper;
+
+/**
+ * 
+ * This class connects to an ActiveMQ instance and creates a session bound to a specific topic,
+ * the {@link ActiveMqConstants#TOPIC_SOCCER_EVENTS}.
+ * {@link LiveUpdatesHelper} will send the events to be written to the topic.
+ * 
+ * @author liako
+ *
+ */
+public class SoccerEventsTopicConsumer 
+implements ExceptionListener, MessageListener {
+	
+	boolean connected = false;
+	
+	Session session;
+	
+	Connection connection;
+
+	MessageConsumer consumer;
+	
+	public SoccerEventsTopicConsumer(){
+		try {
+			connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void connect() throws Exception {
+		
+		try {
+			ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(ActiveMqConstants.CONNECTION_TCP_URL);
+            connection = cf.createConnection();
+            connection.setExceptionListener(this);
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createTopic(ActiveMqConstants.TOPIC_SOCCER_EVENTS);
+		    consumer = session.createConsumer(destination);
+		    consumer.setMessageListener(this);
+		    connected = true;
+		    connection.start();
+		    System.out.println("*******************ACTIVEMQ CONSUMER OK");
+		} catch (JMSException e) {
+			connection.close();
+			connected = false;
+			throw new Exception(e.getMessage());
+		}catch (Exception e) {
+			connection.close();
+			connected = false;
+			throw new Exception(e.getMessage());
+		}finally {
+			
+		}
+		
+	}
+	
+	
+
+	@Override
+	public void onException(JMSException arg0) {
+		try {
+			System.out.println("ACTIVEMQ CONSUMER SHUT DOWN");
+
+			connection.close();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			connected = false;
+		}
+		
+	}
+
+
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			System.out.println("RECEIVED" + message.getObjectProperty("homeScore"));
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
